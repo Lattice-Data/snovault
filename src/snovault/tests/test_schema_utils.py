a7021e7a8ca4dd0693d3abc4ee789108a154a72e
Pedro Assis <pedroh@stanford.edu>
2020-07-22 14:35:10 -0700

    specs

--- src/snovault/tests/test_schema_utils.py
+++ src/snovault/tests/test_schema_utils.py
@@ -32,3 +32,39 @@ def test_uniqueItems_validates_normalized_links(content, threadlocals):
         errors[0].message == "['{}', '{}'] has non-unique elements".format(
             uuid, uuid)
     )
+
+
+def test_pattern_validates_strings_or_array_of_strings(content, threadlocals):
+    schema = {
+        'items': {
+            'pattern': 'option1|option2'
+        }
+    }
+
+    uuid = 'option2'
+    data = [
+        uuid,
+        '/testing-link-targets/{}'.format(uuid),
+    ]
+
+    validated, errors = validate(schema, data)
+    assert len(errors) == 0
+
+    schema['items']['pattern'] = ['option1|', 'option2']
+    validated, errors = validate(schema, data)
+    assert len(errors) == 0
+
+    uuid = 'option3'
+    data = [
+        uuid,
+        '/testing-link-targets/{}'.format(uuid),
+    ]
+
+    validated, errors = validate(schema, data)
+    assert len(errors) > 0
+    assert errors[0].message == "'option3' does not match 'option1|option2'"
+
+    schema['items']['pattern'] = ['option1|', 'option2']
+    validated, errors = validate(schema, data)
+    assert len(errors) > 0
+    assert errors[0].message == "'option3' does not match 'option1|option2'"
